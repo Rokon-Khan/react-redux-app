38473b799e2d7e1a19038caa44d8a24240bfe60b
Md Rokonuzzaman <rokonjust@gmail.com>
2025-06-28 23:05:27 +0600

    Home Page Added

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center mt-10">
+        Welcome to React Redux TypeScript!
+      </h1>
+      <p className="mt-4 text-lg text-center">
+        This is a simple example of a React-Rdux Quiz application using Redux
+        and TypeScript.
+      </p>
+    </div>
+  );
+}
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,9 +1,10 @@
+import Home from "@/pages/Home";
 import { createBrowserRouter } from "react-router";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div className="text-center text-4xl mt-5">React Redux App</div>,
+    element: <Home />,
   },
 ]);
 
